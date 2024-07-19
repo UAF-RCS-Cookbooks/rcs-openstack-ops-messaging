d63dcdf0a17c58145addfccfd90329ae7bbf3746
Jeremiah Dabney <jdabney@gmail.com>
2024-07-18 19:00:24 -0800

    Converting to RCS managed

--- Berksfile
+++ Berksfile
@@ -1,13 +1,4 @@
 source 'https://supermarket.chef.io'
-
-solver :ruby, :required
-
-%w(-common).each do |cookbook|
-  if Dir.exist?("../cookbook-openstack#{cookbook}")
-    cookbook "openstack#{cookbook}", path: "../cookbook-openstack#{cookbook}"
-  else
-    cookbook "openstack#{cookbook}", git: "https://opendev.org/openstack/cookbook-openstack#{cookbook}"
-  end
-end
+source :chef_server
 
 metadata
--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
-name             'openstack-ops-messaging'
-maintainer       'openstack-chef'
-maintainer_email 'openstack-discuss@lists.openstack.org'
+name             'rcs-openstack-ops-messaging'
+maintainer       'UAF RCS'
+maintainer_email 'chef@rcs.alaska.edu'
 license          'Apache-2.0'
 description      'Provides the shared messaging configuration for Chef for OpenStack.'
 version          '20.0.0'
@@ -9,9 +9,7 @@ version          '20.0.0'
   supports os
 end
 
-depends 'openstack-common', '>= 20.0.0'
+depends 'rcs-openstack-common', '>= 20.0.0'
 depends 'rabbitmq', '>= 5.8.5'
 
-issues_url 'https://launchpad.net/openstack-chef'
-source_url 'https://opendev.org/openstack/cookbook-openstack-ops-messaging'
 chef_version '>= 16.0'
