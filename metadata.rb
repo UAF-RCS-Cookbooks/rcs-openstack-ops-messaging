786b894168340993fb43f0745f11009d8172046f
Jeremiah Dabney <jdabney@gmail.com>
2024-08-16 16:45:52 -0800

    Had to change rabbitmq version

--- metadata.rb
+++ metadata.rb
@@ -3,13 +3,13 @@ maintainer       'UAF RCS'
 maintainer_email 'chef@rcs.alaska.edu'
 license          'Apache-2.0'
 description      'Provides the shared messaging configuration for Chef for OpenStack.'
-version          '21.0.0'
+version          '21.0.1'
 
 %w(ubuntu redhat centos).each do |os|
   supports os
 end
 
 depends 'rcs-openstack-common', '>= 20.0.0'
-depends 'rabbitmq', '>= 5.10.0'
+depends 'rabbitmq', '>= 5.9.1'
 
 chef_version '>= 16.0'
